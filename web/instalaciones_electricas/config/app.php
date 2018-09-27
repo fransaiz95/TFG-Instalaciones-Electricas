d464bcf02b470c0268942974ee854d7ebc3093ae
fransaiz95 <fsg0016@alu.ubu.es>
2018-09-27 21:28:05 +0200

    Configuracion de la base de datos.
    Cambios en el fichero de configuracion para conectar correctamente con la base de datos MySQL creada

--- web/instalaciones_electricas/config/app.php
+++ web/instalaciones_electricas/config/app.php
@@ -230,9 +230,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'Fran1234',
+            'password' => 'Fran1234',
+            'database' => 'instalaciones_electricas',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
