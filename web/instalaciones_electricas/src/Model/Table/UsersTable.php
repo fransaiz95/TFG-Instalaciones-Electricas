e496d8d37fb42df788eed69f0a030e1194fc3c3a
fransaiz95 <fsg0016@alu.ubu.es>
2019-02-12 23:43:30 +0100

    - Users: Añadidas validaciones para la contraseña: Minimo 8 caracteres con 1 numero, 1 mayuscula, 1 minuscula. No puede haber dos usuarios con el mismo username.

--- web/instalaciones_electricas/src/Model/Table/UsersTable.php
+++ web/instalaciones_electricas/src/Model/Table/UsersTable.php
@@ -28,9 +28,40 @@ class UsersTable extends Table
             ->notEmpty('surname', 'A surname is required')
             ->notEmpty('username', 'A username is required')
             ->notEmpty('password', 'A password is required')
-            ->notEmpty('id_role', 'A role is required');
+            ->notEmpty('id_role', 'A role is required')
+            ->add('password', 'custom', [
+                'rule' => [$this, 'checkCharacters'],
+                'message' => 'The password must contain 1 number, 1 uppercase and 1 lowercase.'
+            ])
+            ->add('password', [
+                'length' => [
+                    'rule' => ['minLength', 8],
+                    'message' => 'Passwords must be at least 8 characters long.',
+                ]
+            ])
+            ->add('username',['unique' => ['rule' => 'validateUnique', 'provider' => 'table', 'message' => 'That user already exists']]);
     }
 
+    public function checkCharacters($password, array $context)
+    {
+        // number
+        if (!preg_match("#[0-9]#", $password)) {
+            return false;
+        }
+        // Uppercase
+        if (!preg_match("#[A-Z]#", $password)) {
+            return false;
+        }
+        // lowercase
+        if (!preg_match("#[a-z]#", $password)) {
+            return false;
+        }
+        // // special characters
+        // if (!preg_match("#\W+#", $password) ) {
+        //     return false;
+        // }
+        return true;
+    }
 
     public function getQueryUsersAndRole ($filters){
 		$query = $this->find('all');
